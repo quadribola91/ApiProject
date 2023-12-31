3a08ea89412340ba1d0c1b60361911642d5a1a1a
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2023-12-31 16:13:17 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import UserList from "./UserList";
+
+export default function App() {
+  return (
+    <>
+      <UserList />
+    </>
+  );
+}
---
+++ src/UserList.js
@@ -0,0 +1,35 @@
+// UserList.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once after the initial render.
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {listOfUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
